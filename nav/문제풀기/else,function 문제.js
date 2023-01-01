32e2c4c6a32aee7bbdcee9e4cd4a63b3b485ccc6
kdb <dabin6743@gmail.com>
2023-01-01 23:12:34 +0900

    함수 문제풀기

--- nav/문제풀기/else,function 문제.js
+++ nav/문제풀기/else,function 문제.js
@@ -23,3 +23,16 @@ function 합격했냐(num1,num2){
 }
 
 합격했냐(30,80);
+
+/*가격을 파라미터로 입력하면 10% 할인가를 뱉는 함수를 만들어봅시다.
+첫 구매여부도 true/false로 둘째파라미터에 입력해서 첫 구매가 맞을 경우 추가로 1.5 달러도 할인해줘야합니다.*/
+
+function 햠수(c,d){
+    var price = c - (c * 0.1);
+    if (d==true){
+        price = price - 1.5
+    }
+    return price
+}
+
+console.log(햠수(90,true)) 
