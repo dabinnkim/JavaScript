c3e68adfc1f7361e2f772e1d5c38e7fc19895c89
kdb <dabin6743@gmail.com>
2023-01-04 17:53:34 +0900

    이벤트 버블링 응용, dataset

--- js실습/ch02/tab.js
+++ js실습/ch02/tab.js
@@ -1,4 +1,7 @@
-/*
+/* tab1
+
+♣for 반복문 사용해서 tab 만들기
+
 버튼0 누르면 
 2. 모든버튼에 orange 클래스명 제거
 1. 버튼0에 orange 클래스명 부착
@@ -11,17 +14,19 @@
 var tab_btn = $('.tab-button'); //자주쓰는 셀렉터는 변수에 넣어 쓰기(성능적으로 좋음)
 var tab_con = $('.tab-content');
 
-for(let i =0; i<tab_btn.length; i++){ //찾으려는 요소 뒤에 .length 붙이면 갯수 세어줌 
+for(let i =0; i<tab_btn.length; i++){ //찾으려는 요소 뒤에 .length 붙이면 요소의 갯수 세어줌 
 // for(let i = 0; i<5; i++){ //버튼이 5개 이므로 반복문도 5번돌고 멈춤, 반복문 쓸때 let 넣어야 정확히 반복됨
     tab_btn.eq(i).on('click',function(){ // .eq(n) : 왼쪽의 n번째 
-        tab_btn.removeClass('orange');
-        tab_btn.eq(i).addClass('orange');
-        tab_con.removeClass('show');
-        tab_con.eq(i).addClass('show');
+        tabopen(i)
     });
 }
 
-
+function tabopen(num){ //아래 코드 너무 길어서 함수로 축약함
+    tab_btn.removeClass('orange');
+    tab_btn.eq(num).addClass('orange');
+    tab_con.removeClass('show');
+    tab_con.eq(num).addClass('show');
+}
 
 
 
@@ -45,3 +50,28 @@ for(let i =0; i<tab_btn.length; i++){ //찾으려는 요소 뒤에 .length 붙�
 //     $('.tab-content').removeClass('show');
 //     $('.tab-content').eq(2).addClass('show');
 // })
+
+
+
+
+
+
+/* tab2
+
+♣이벤트리스너 하나만 써서 tab 만들기
+이벤트 버블링 알고있으면 이벤트리스너 많이 필요 없음
+*/
+var btn = $('.tab-btn');
+var con = $('.tab-con');
+
+function tabopen2(num){
+    btn.removeClass('blue');
+    btn.eq(num).addClass('blue');
+    con.removeClass('show');
+    con.eq(num).addClass('show');
+}
+
+$('.li').click(function(e){
+    console.log(e.target.dataset.id);
+    tabopen2(e.target.dataset.id); //지금 누른 버튼에 숨겨져있던 data-id 가져와줌
+})
